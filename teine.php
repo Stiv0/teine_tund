b28dd1bf0e50d30f0b21e5429a0f2d390fa7b99d
stivsire <stivsire@greeny.cs.tlu.ee>
2016-09-14 14:49:08 +0300

    added if and for loop

---
+++ teine.php
@@ -0,0 +1,27 @@
+<?php
+
+	$age = 5;
+	if ($age <= 17) {
+		
+		echo "alaealine";
+		
+	}	else {
+		
+		echo "täisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php
+
+	for($i = 1; $i <= $age; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	}
+	
+	echo "õnne";
+?>
